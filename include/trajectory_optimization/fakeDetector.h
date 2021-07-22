14279a6872dd2d0061243c4f5ccde508da6013f4
Zhefan-Xu <zhefanx@andrew.cmu.edu>
2021-07-22 23:07:27 +0800

    CMake

--- include/trajectory_optimization/fakeDetector.h
+++ include/trajectory_optimization/fakeDetector.h
@@ -1,1 +1,13 @@
+#ifndef FAKEDETECTOR_H
+#define FAKEDETECTOR_H
 #include <trajectory_optimization/fakeDetector.h>
+
+// this class is for simulation 
+class fakeDetector{
+private:
+
+public:
+	fakeDetector(const ros::NodeHandle& _nh, std::string topicName);
+};
+
+#endif
