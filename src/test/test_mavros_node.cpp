d27a1f6e9e117c415f5537ad4996bb4b485a04bc
Zhefan-Xu <zhefanx@andrew.cmu.edu>
2021-08-07 23:13:26 +0800

    box experiment

--- src/test/test_mavros_node.cpp
+++ src/test/test_mavros_node.cpp
@@ -4,8 +4,9 @@
 int main(int argc, char** argv){
 	ros::init(argc, argv, "test_mavros_node");
 	ros::NodeHandle nh;
-	std::string filename = "/home/zhefan/catkin_ws/src/trajectory_optimization/path/waypoint_maze_complete.txt";
-	int idx = 37; double delT = 0.1;
+	// std::string filename = "/home/zhefan/catkin_ws/src/trajectory_optimization/path/waypoint_maze_complete.txt"; int idx = 37; 
+	std::string filename = "/home/zhefan/catkin_ws/src/trajectory_optimization/path/waypoint_box.txt"; int idx = 0;
+	double delT = 0.1;
 	mavrosTest mtest (nh, delT);
 	mtest.loadPath(filename, idx);
 	mtest.setInitialPosition();
