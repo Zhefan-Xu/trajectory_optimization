ec441bc5976b4f344947f418310a14260f616c9e
Zhefan-Xu <zhefanx@andrew.cmu.edu>
2021-08-17 13:01:24 +0800

    tree env

--- src/test/test_mavros_node.cpp
+++ src/test/test_mavros_node.cpp
@@ -7,7 +7,8 @@ int main(int argc, char** argv){
 	// std::string filename = "/home/zhefan/catkin_ws/src/trajectory_optimization/path/waypoint_maze_complete.txt"; int idx = 37; std::vector<std::string> obstaclesType {"person_walking", "person_walking_0"};
 	// std::string filename = "/home/zhefan/catkin_ws/src/trajectory_optimization/path/waypoint_box.txt"; int idx = 0; std::vector<std::string> obstaclesType {"person_walking", "person_walking_0"};
 	// std::string filename = "/home/zhefan/catkin_ws/src/trajectory_optimization/path/waypoint_maze.txt"; int idx = 0;  std::vector<std::string> obstaclesType {"person_walking", "person_walking_0"};
-	std::string filename = "/home/zhefan/catkin_ws/src/trajectory_optimization/path/waypoint_tunnel.txt"; int idx = 0;  std::vector<std::string> obstaclesType {"person_walking", "warehouse_robot", "warehouse_robot_0"};
+	// std::string filename = "/home/zhefan/catkin_ws/src/trajectory_optimization/path/waypoint_tunnel.txt"; int idx = 0;  std::vector<std::string> obstaclesType {"person_walking", "warehouse_robot", "warehouse_robot_0"};
+	std::string filename = "/home/zhefan/catkin_ws/src/trajectory_optimization/path/waypoint_tree.txt"; int idx = 0;  std::vector<std::string> obstaclesType {"person_walking", "person_walking_0", "person_walking_1", "person_walking_2", "person_walking_3"};
 
 	double delT = 0.1;
 	mavrosTest mtest (nh, delT);
